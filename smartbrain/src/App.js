25c4c0befebdbb7e3fdb208eade068009db038a9
alihassan-here <alihassanuog50@gmail.com>
2022-06-09 11:27:45 +0500

    completed basic recognition

--- smartbrain/src/App.js
+++ smartbrain/src/App.js
@@ -4,7 +4,16 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+
+
+
+const app = new Clarifai.App({
+  apiKey: process.env.REACT_APP_API_KEY
+});
+
 
 
 const particlesOptions = {
@@ -24,6 +33,49 @@ const particlesOptions = {
 class App extends Component {
   constructor() {
     super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+    }
+  }
+
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  }
+
+  calculateFaceLocation = data => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputImage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = box => {
+    this.setState({ box });
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    this.setState({ imageUrl: this.state.input });
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.input
+      )
+      .then(response => {
+        this.displayFaceBox(this.calculateFaceLocation(response));
+        this.setState({ input: '' });
+      }
+      )
+      .catch(err => console.log(err));
   }
 
   render() {
@@ -36,7 +88,8 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit} />
+        <FaceRecognition imageUrl={this.state.imageUrl} box={this.state.box} />
       </div>
     );
   }
--- smartbrain/src/components/Logo/Logo.jsx
+++ smartbrain/src/components/Logo/Logo.jsx
@@ -7,7 +7,7 @@ import brain from "./brain.png";
 
 const Logo = () => {
     return (
-        <div className="ma4 mt0">
+        <div className="ml4 mt0">
             <Tilt className="Tilt br2 shadow-2" options={{ max: 55 }} style={{ height: 150, width: 150 }} >
                 <div className="Tilt-inner pa2"> <img src={brain} alt="brain" />
                 </div>
